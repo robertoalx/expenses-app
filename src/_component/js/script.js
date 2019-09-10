cd660e5c38c4fb2dfbdea0374265567a2337fac7
Robert Alexandru <romaniarobertverde@yahoo.com>
2019-09-10 23:30:28 +0100

    we just created the first array with multiple objects inside.

--- src/_component/js/script.js
+++ src/_component/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+let payment = [
+  {
+  date: "01-09-2019",
+  reason: "rent",
+  moneySpent: 300
+  },
+  {
+  date: "02-09-2019",
+  reason: "Mc",
+  moneySpent: 31
+  },
+  {
+  date: "01-09-2019",
+  reason: "rent",
+  moneySpent: 31
+  },
+  {
+  date: "01-09-2019",
+  reason: "rent",
+  moneySpent: 31
+  },
+];
+console.log(payment);
